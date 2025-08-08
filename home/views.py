10ccf57162f872a62c11d8ddb3f395f64db5e048
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-08 16:45:03 +0530

    22411

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    """
+    Renders the main homepage template.
+
+    """
+    return render(request,'home.html')
