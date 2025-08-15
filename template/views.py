8c4e658bf4513241a0c20ad934a1a726d752fda3
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-15 20:42:35 +0530

    32808

--- template/views.py
+++ template/views.py
@@ -1,11 +1,15 @@
 #views.py
 
 from django.shortcuts import render
+from django.http import HttpResponseServerError
 
 
 def home(request):
-    context = {
+    try:
+        context = {
         'restaurant_name' : 'Flavours of India'
 
-    }
-    return render(request, 'home.html',context)
+        }
+        return render(request, 'home.html',context)
+    except Exception as e:
+        return HttpResponseServerError(f"An error occured: {e}")
