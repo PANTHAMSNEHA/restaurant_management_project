537df03c742c1406aeaccf34c9f3e5041e65f99f
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-19 17:46:29 +0530

    34451

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,10 +1,8 @@
+import requests
+from django.shortcuts import render
+
 def home(request):
-    try:
-        query = request.GET.get('q','')
-        content = {
-            "restaurant_name":"Flavours of India",
-            "search_query": query
-        }
-        return render(request, 'home.html', content)
-    except Exception as e:
-        return HttpResponseServerErrir(f"An error occurred: {e}")
+    response = requests.get('https://your-api-url.com/menu/')  #Replace with actual api
+    menu_data = response.json() if response.status_code ==200 else []
+    return render(request, 'restaurant_app/home.html',{'menu': menu_data})
+    
