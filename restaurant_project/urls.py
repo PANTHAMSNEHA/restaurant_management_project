7f86eb21bd904bdfe1f750fe66f5b6e69a1288c8
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-26 23:43:16 +0530

    38785

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path
-from .views import homepage_view
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpattern = [
-    path('', homepage_view, name='homepage'),
-]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
