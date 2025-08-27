26e8e392081b120ba5bc3c7a13fc31a12eabe558
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-27 15:02:35 +0530

    39648

---
+++ restaurant_project/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Restaurant
+
+def homepage(request):
+    restaurant = Restaurant.objects.first()
+    return render(request, 'homepage.html',{'restaurant':restaurant})
