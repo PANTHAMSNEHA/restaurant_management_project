c6e49e6b0edd32dbd6a72478168c31dc52dcd47d
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-28 20:56:15 +0530

    39647

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -5,7 +5,22 @@ class RestaurantLocation(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=10)
+    
 
+    description = models.TextField()
+
+    #Opening hours stored as a JSON field(dictionary format)
+    opening_hours = models.JSONField(default=dict)
+
+    opening_hours={
+        "Monday": "9.00AM - 9:00PM",
+        "Tuesday": "9.00 AM - 9:00 PM",
+        "Wednesday":"Closed",
+        "Thursday":"9.00 AM - 9.00PM",
+        "Friday":"9.00 AM - 10.00PM",
+        "Saturday": "10:00 AM - 10:00PM"
+        "Sunday":"10:00 AM - 8:00 PM"
+    }
 
     def __str__(self):
         return f"{self.address},{self.city}, {self.state} {self.zip_code}"
