35c1855cc4aa2cd325dd4c03f7c7a7bb982d2623
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-28 21:24:11 +0530

    39646

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,8 +1,14 @@
 import requests
 from django.shortcuts import render
+from .models import MenuItem
+
 
 def home(request):
     response = requests.get('https://your-api-url.com/menu/')  #Replace with actual api
     menu_data = response.json() if response.status_code ==200 else []
     return render(request, 'restaurant_app/home.html',{'menu': menu_data})
     
+
+def menu_view(request):
+    items = MenuItem.objects.all()
+    return render(request, 'menu.html',{'items':items})
