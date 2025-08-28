a2385804e19f246e050668f176e80043c8e964b8
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-28 21:25:51 +0530

    39646

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -6,6 +6,9 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.CharField(max_lengths=50)
 
+    #New image field
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+
     def __str__(self):
         return self.name
 
