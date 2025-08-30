bd37379cb023f7223fe03a8daf114d9a6764f23a
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-08-30 19:39:03 +0530

    40262

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -1,11 +1,8 @@
 import os
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#Also add this to your main urls.py
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(sttings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#Add to INSTALLED_APPs if not already
+INSTALLED_APPS += ['restaurant']
