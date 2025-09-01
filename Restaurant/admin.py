9ee18f29a71fc260eedd915d8d1cd02ea572e0b7
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-09-01 21:37:42 +0530

    40895

--- Restaurant/admin.py
+++ Restaurant/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import MenuItem
+from you_app.models import RestaurantInfo
+RestaurantInfo.objects.create(phone="+91-9234567789")
 
 admin.site.register(MenuItem)
