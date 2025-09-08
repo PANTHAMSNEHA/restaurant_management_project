87955906f5d39db22a17c9956fe89e2cb73395d9
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-09-08 09:32:04 +0530

    42197

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -1,6 +1,11 @@
 from django.conf import path
 from . import views
+from django.urls import path
+from .views import order_confirmation
+
 
 urlpatterns = [
     path('',views.homepage, name='homepage'),
+    path('admin/',admin.site.urls),
+    path('orders/', include('orders.urls')), #now accessible at /orders/confirm/
 ]
