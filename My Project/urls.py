6f65421ca51243d985e160b0b80ea71b6040d6cc
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-09-08 08:42:44 +0530

    42196

--- My Project/urls.py
+++ My Project/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import login_view
 
 urlpatterns = [
     path('',views.home, name="home"),
     path('contact/',views,contact, name='contact'),
     path('feedback/',views.feedback, name='feedback'),
+    path('login/',login_view, name='login')
 ]
