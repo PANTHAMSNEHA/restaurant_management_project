140f62d6fac57797d3d7d2568e62774a4295af94
PANTHAMSNEHA <155801471+PANTHAMSNEHA@users.noreply.github.com>
2025-09-11 16:16:53 +0530

    43376

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -5,3 +5,6 @@ def homepage(request):
     restaurant = Restaurant.objects.first()
     return render(request, 'homepage.html',{'restaurant':restaurant})
     
+
+def homepage_view(request):
+    return render(request, 'home.html')
